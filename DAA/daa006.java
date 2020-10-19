e0f5cb163ea6c73bc5504f1a634c3206fe1f51a9
Luis Duarte <luis99freitas@gmail.com>
2020-10-19 17:41:19 +0100

    TW delivery 1 (work struct)

---
+++ DAA/daa006.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class daa006 {
+    FastScanner in = new FastScanner(System.in);
+
+    int n = in.nextInt; // número de casos
+
+    for(int i = 0; i < n; i++ ) {
+        int qx = in.nextInt(); // qx e qx representa o canto inferior esquerdo de cada quadrado
+        int qy = in.nextInt();
+        int q1 = in.nextInt(); // q1 representa o tamanho de cada lado do quadrado
+
+        int cx = in.nextInt(); // cx e cy representam o centro do círculo
+        int cy = in.nextInt();
+        int cr = in.nextInt(); // cr representa o raio do círculo
+        
+    }
+}
