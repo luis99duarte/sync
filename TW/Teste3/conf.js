e7b2689dca66b86d6008d94e122d07da54a887b4
Luis Duarte <luis99freitas@gmail.com>
2020-12-23 15:25:41 +0000

    Teste entrega 3 TW
    conf.js

---
+++ TW/Teste3/conf.js
@@ -0,0 +1,12 @@
+module.exports.documentRoot = '';               // Diretório que a raiz dos documentos (não sei qual é, nem como é feita a divisão de diretórios)
+module.exports.defaultIndex = 'index.html';     // Ficheiro html
+module.exports.port = 8104;                     // Porta 81XX sendo XX = 04 que é o número do grupo
+module.exports.mediaTypes = {                   // Código dado pelo Prof no slide das configurações dos exemplos do servidor estático
+    'txt':      'text/plain',
+    'html':     'text/html',
+    'css':      'text/css',
+    'js':       'application/javascript',
+    'png':      'image/png',
+    'jpeg':     'image/jpeg',
+    'jpg':      'image/jpeg',
+}
